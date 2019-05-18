bd6ba9167dc03b86e997dafe9c030f8c3d56d9e7
Jonathan Camenisch <jonathan@camenisch.net>
2019-05-17 23:56:15 -0400

    More hammerspoon!

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,103 +1,57 @@
+-- -------------------------------
+-- Window management shortcut keys
+-- -------------------------------
+
 hs.window.animationDuration = 0
 hs.grid.setGrid('12x12').setMargins({0, 0})
 
-function windowSizer(fn)
-  return function()
-    local win = hs.window.focusedWindow()
-    local screenFrame = fn(win:screen():frame())
+-- Fill current screen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", function()
+  local win = hs.window.focusedWindow()
+  win:setFrame(win:screen():frame())
+end)
 
-    win:setFrame(hs.geometry.new(screenFrame))
-  end
-end
+-- Vertical halves
+hs.hotkey.bind({'alt', 'ctrl'}, 'Left',  function() hs.grid.set(hs.window.focusedWindow(), '0,0 6x12') end)
+hs.hotkey.bind({'alt', 'ctrl'}, 'Right', function() hs.grid.set(hs.window.focusedWindow(), '6,0 6x12') end)
 
+-- Horizontal halves
+hs.hotkey.bind({'alt', 'ctrl'}, 'Up',   function() hs.grid.set(hs.window.focusedWindow(), '0,0 12x6') end)
+hs.hotkey.bind({'alt', 'ctrl'}, 'Down', function() hs.grid.set(hs.window.focusedWindow(), '0,6 12x6') end)
+
+-- Vertical thirds
+hs.hotkey.bind({'alt', 'ctrl'}, ',', function() hs.grid.set(hs.window.focusedWindow(), '0,0 4x12') end)
+hs.hotkey.bind({'alt', 'ctrl'}, '.', function() hs.grid.set(hs.window.focusedWindow(), '4,0 4x12') end)
+hs.hotkey.bind({'alt', 'ctrl'}, '/', function() hs.grid.set(hs.window.focusedWindow(), '8,0 4x12') end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, ';', function() hs.grid.set(hs.window.focusedWindow(), '0,0 8x12') end)
+hs.hotkey.bind({'alt', 'ctrl'}, "'", function() hs.grid.set(hs.window.focusedWindow(), '4,0 8x12') end)
+
+-- Quadrants
+hs.hotkey.bind({'alt', 'ctrl'}, '-', function() hs.grid.set(hs.window.focusedWindow(), '0,0 6x6') end)
+hs.hotkey.bind({'alt', 'ctrl'}, '=', function() hs.grid.set(hs.window.focusedWindow(), '6,0 6x6') end)
+hs.hotkey.bind({'alt', 'ctrl'}, '[', function() hs.grid.set(hs.window.focusedWindow(), '0,6 6x6') end)
+hs.hotkey.bind({'alt', 'ctrl'}, ']', function() hs.grid.set(hs.window.focusedWindow(), '6,6 6x6') end)
+
+-- Move the current window to a different monitor
 function switchScreen(direction)
   return function()
     local win = hs.window.focusedWindow()
     local cell = hs.grid.get(win)
     local screen = win:screen()
 
-    if direction == "East" then screen = screen:toEast()
-    elseif direction == "West" then screen = screen:toWest()
-    elseif direction == "North" then screen = screen:toNorth()
-    elseif direction == "South" then screen = screen:toSouth()
-    end
-
-    hs.grid.set(win, cell, win:screen():next())
+    hs.grid.set(win, cell, screen[direction](screen))
   end
 end
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', switchScreen('toEast'))
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', switchScreen('toWest'))
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Up', switchScreen('toNorth'))
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Down', switchScreen('toSouth'))
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", windowSizer(function(screen)
-  return screen
-end))
 
--- -------------------------------
--- Window management shortcut keys
--- -------------------------------
-
--- Vertical halves
-hs.hotkey.bind({"alt", "ctrl"}, "Left", function()
-  hs.grid.set(hs.window.focusedWindow(), '0,0 6x12')
-end)
-
-hs.hotkey.bind({"alt", "ctrl"}, "Right", function()
-  hs.grid.set(hs.window.focusedWindow(), '6,0 6x12')
-end)
-
--- hs.hotkey.bind({"alt", "ctrl"}, "Left", windowSizer(function(screen)
---   return { screen.x, screen.y, screen.w / 2 + 2, screen.h }
--- end))
---
--- hs.hotkey.bind({"alt", "ctrl"}, "Right", windowSizer(function(screen)
---   return { screen.w / 2 - 1, screen.y, screen.w / 2 + 2, screen.h }
--- end))
-
--- Horizontal halves
-hs.hotkey.bind({"alt", "ctrl"}, "Up", function()
-  hs.grid.set(hs.window.focusedWindow(), '0,0 12x6')
-end)
-
-hs.hotkey.bind({"alt", "ctrl"}, "Down", function()
-  hs.grid.set(hs.window.focusedWindow(), '0,6 12x6')
-end)
-
--- hs.hotkey.bind({"alt", "ctrl"}, "Up", windowSizer(function(screen)
---   return { screen.x, screen.y, screen.w, screen.h / 2 + 8 }
--- end))
---
--- hs.hotkey.bind({"alt", "ctrl"}, "Down", windowSizer(function(screen)
---   return { screen.x, screen.y + screen.h / 2 - 4, screen.w, screen.h / 2 + 8 }
--- end))
-
--- Vertical thirds
-hs.hotkey.bind({"alt", "ctrl"}, "i", function()
-  hs.grid.set(hs.window.focusedWindow(), '0,0 4x12')
-end)
-
-hs.hotkey.bind({"alt", "ctrl"}, "o", function()
-  hs.grid.set(hs.window.focusedWindow(), '4,0 4x12')
-end)
-
-hs.hotkey.bind({"alt", "ctrl"}, "p", function()
-  hs.grid.set(hs.window.focusedWindow(), '8,0 4x12')
-end)
-
--- hs.hotkey.bind({"alt", "ctrl"}, "i", windowSizer(function(screen)
-  -- return { screen.x, screen.y, screen.w / 3 + 1.5, screen.h }
--- end))
-
--- hs.hotkey.bind({"alt", "ctrl"}, "o", windowSizer(function(screen)
-  -- return { screen.w / 3, screen.y, screen.w / 3 + 1.5, screen.h }
--- end))
-
--- hs.hotkey.bind({"alt", "ctrl"}, "p", windowSizer(function(screen)
-  -- return { screen.w * 2 / 3, screen.y, screen.w / 3 + 1.5, screen.h }
--- end))
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", switchScreen("East"))
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", switchScreen("West"))
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", switchScreen("North"))
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", switchScreen("South"))
-
+-- -----------
+-- Bookkeeping
+-- -----------
 
 function reloadConfig(files)
   doReload = false
