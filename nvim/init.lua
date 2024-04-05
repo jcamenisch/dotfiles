6613c5a5738397520a8ed5ef2bdc4e6eda771833
Jonathan Camenisch <jonathan@camenisch.net>
2024-04-05 16:07:19 -0400

    Enable a few more formatting features

--- nvim/init.lua
+++ nvim/init.lua
@@ -539,7 +539,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -548,7 +548,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
@@ -631,6 +631,8 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        go = { 'goimports', 'gofmt' },
+        ['_'] = { 'trim_whitespace' },
       },
     },
   },
