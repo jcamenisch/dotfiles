dd76aee018f7524d731da4a9e0d1730f13cfc3c5
Jonathan Camenisch <jonathan@camenisch.net>
2024-04-10 16:35:21 -0400

    Enable nerdfont in nvim; Add copilot plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -234,6 +234,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  'github/copilot.vim', -- Github Copilot
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
