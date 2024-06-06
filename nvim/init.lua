ca61abb9748c02b12480ff098bf2d50692e1ad6e
jcamenisch <jonathan@camenisch.net>
2024-06-06 16:55:33 -0500

    checkpoint

--- nvim/init.lua
+++ nvim/init.lua
@@ -263,7 +263,14 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  'github/copilot.vim', -- Github Copilot
+  {
+    'supermaven-inc/supermaven-nvim', -- Github Copilot
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
+
+  'https://github.com/andreshazard/vim-freemarker', -- Freemarker template syntax highlighting
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
